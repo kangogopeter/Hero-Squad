466c2ea2c67a87de72eca23346e336324c9eadd0
kangogopeter <peterkangogo501@gmail.com>
2019-05-19 12:03:43 +0300

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+public class App {
+  public static void main(String[] args){
+staticFileLocation("/public");
+String layout = "templates/layout.vtl";
+
+}
+  }
