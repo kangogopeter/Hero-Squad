9b29134c7fce160960848f0c3f34475ee07fb187
kangogopeter <peterkangogo501@gmail.com>
2019-05-19 23:49:59 +0300

    styling the WebApp in style.css

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,11 +1,11 @@
-public class Squad{
+public class Hero{
 
 private String mName;
 private int mSize;
 private String mPersonal;
 private String mGroup;
 
-  public Squad(String name, Integer size, String personal, String group){
+  public Hero(String name, Integer size, String personal, String group){
 
 mName = name;
 mSize = size;
