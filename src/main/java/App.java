8223990d02f1670aa28ea12b1f231bc433f589ff
kangogopeter <peterkangogo501@gmail.com>
2019-05-21 21:13:13 +0300

    initials redone

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,14 +21,31 @@ public class App {
 
 staticFileLocation("/public");
 String layout = "templates/layout.vtl";
+get("/", (request, response) ->{
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("template", "templates/form.vtl");
+    return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
 
- get("/",(request,response) ->{
+        get("/menu",(request,response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/hero.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+ get("/local",(request,response) ->{
 Map<String, Object> model = new HashMap<String, Object>();
  model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+
+
+
  post("/success", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+
+
 ArrayList<Hero> hero=request.session().attribute("hero");
 
  if (hero == null) {
@@ -42,7 +59,7 @@ ArrayList<Hero> hero=request.session().attribute("hero");
             String group = request.queryParams("group");
             Hero newHero = new Hero(name, size, personal, group);
             hero.add(newHero);
-            model.put("hero", request.session().attribute("Squad"));
+            model.put("Hero", request.session().attribute("hero"));
             model.put("template","templates/success.vtl");
 
             return new ModelAndView(model, layout);
