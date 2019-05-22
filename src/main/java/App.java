c934a57a4698e861fb3e8d7e80bdc228efdb6923
kangogopeter <peterkangogo501@gmail.com>
2019-05-22 10:45:17 +0300

    new addition of model.put(hero)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.lang.String;
 import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
 
@@ -17,35 +16,27 @@ public class App {
             port = 4567;
         }
 
-        port(port);
-
+        setPort(port);
 staticFileLocation("/public");
 String layout = "templates/layout.vtl";
-get("/", (request, response) ->{
-    Map<String, Object> model = new HashMap<String, Object>();
-    model.put("template", "templates/form.vtl");
-    return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
-
-        get("/menu",(request,response) ->{
-            Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "templates/hero.vtl");
-            return new ModelAndView(model, layout);
-        }, new VelocityTemplateEngine());
 
  get("/local",(request,response) ->{
 Map<String, Object> model = new HashMap<String, Object>();
  model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
-
-
-
-
+        get("/menu",(request,response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+             model.put("template", "templates/hero.vtl");
+                        return new ModelAndView(model, layout);
+                    }, new VelocityTemplateEngine());
+                    get("/",(request,response) ->{
+                        Map<String, Object> model = new HashMap<String, Object>();
+                         model.put("template", "templates/form.vtl");
+                                    return new ModelAndView(model, layout);
+                                }, new VelocityTemplateEngine());
  post("/success", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-
-
 ArrayList<Hero> hero=request.session().attribute("hero");
 
  if (hero == null) {
@@ -59,9 +50,8 @@ ArrayList<Hero> hero=request.session().attribute("hero");
             String group = request.queryParams("group");
             Hero newHero = new Hero(name, size, personal, group);
             hero.add(newHero);
-            model.put("Hero", request.session().attribute("hero"));
+            model.put("hero", hero);
             model.put("template","templates/success.vtl");
-
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
